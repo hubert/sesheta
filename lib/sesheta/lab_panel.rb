f5a4829bcbf2842943841af4509acdf53e8bb8cb
Hubert Huang <hhuang@practicefusion.com>
2013-09-10 20:27:25 -0700

    Fake associations by passing db connection around and abstract stored procedure calls within methods

--- lib/sesheta/lab_panel.rb
+++ lib/sesheta/lab_panel.rb
@@ -1,5 +1,14 @@
-class Sesheta::LabPanel < Hashie::Trash
-  property :id, :from => :LabPanelID
-  property :name, :from => :PanelName
-  property :status, :from => :Status
+module Sesheta
+  class LabPanel < Hashie::Trash
+    property :id, :from => :LabPanelID
+    property :name, :from => :PanelName
+    property :status, :from => :Status
+    property :connection # yes, this is a connection to the db. FML
+
+    def lab_observations
+      connection.execute_procedure('phr_GetLabObservations', id).map do |lab_observation|
+        LabObservation.new(lab_observation)
+      end
+    end
+  end
 end
--- lib/sesheta/lab_result.rb
+++ lib/sesheta/lab_result.rb
@@ -1,6 +1,15 @@
-class Sesheta::LabResult < Hashie::Trash
-  property :id, :from => :LabResultID
-  property :report_date, :from => :ReportDate
-  property :provider_first_name, :from => :ProviderFirstName
-  property :provider_last_name, :from => :ProviderLastName
+module Sesheta
+  class LabResult < Hashie::Trash
+    property :id, :from => :LabResultID
+    property :report_date, :from => :ReportDate
+    property :provider_first_name, :from => :ProviderFirstName
+    property :provider_last_name, :from => :ProviderLastName
+    property :connection # yes, this is a connection to the db. FML
+
+    def lab_panels
+      connection.execute_procedure('phr_GetLabPanels', id).map do |lab_panel|
+        LabPanel.new(lab_panel.merge(:connection => connection))
+      end
+    end
+  end
 end
