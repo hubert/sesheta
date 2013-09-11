6ab7a840cb0f546ea0019db1a4591fc81d7b9e90
Hubert Huang <hhuang@practicefusion.com>
2013-09-11 14:35:34 -0700

    Add Medication#prescriptions

--- lib/sesheta/medication.rb
+++ lib/sesheta/medication.rb
@@ -1,9 +1,18 @@
-class Sesheta::Medication < Hashie::Trash
-  property :name, :from => :MedName
-  property :strength, :from => :Strength
-  property :id, :from => :MedId
-  property :first_name, :from => :FirstName
-  property :last_name, :from => :LastName
-  property :sig_provider, :from => :SigProvider
-  property :sig_patient, :from => :SigPatient
+module Sesheta
+  class Medication < Hashie::Trash
+    property :name, :from => :MedName
+    property :strength, :from => :Strength
+    property :id, :from => :MedId
+    property :first_name, :from => :FirstName
+    property :last_name, :from => :LastName
+    property :sig_provider, :from => :SigProvider
+    property :sig_patient, :from => :SigPatient
+    property :connection
+
+    def prescriptions
+      connection.execute_procedure('phr_GetPrescriptions', id).map do |prescription|
+        Prescription.new(prescription)
+      end
+    end
+  end
 end
