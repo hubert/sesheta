3c7a7f7ec9fb21d91bf096ff5ea0cc55ab541b79
Hubert Huang <hhuang@practicefusion.com>
2013-09-16 18:12:51 -0700

    Remove first_name and last_name properties from Visit

--- lib/sesheta/diagnosis.rb
+++ lib/sesheta/diagnosis.rb
@@ -1,8 +1,6 @@
 class Sesheta::Diagnosis < Hashie::Trash
   property :icd, :from => :ICD
   property :dx_name, :from => :DxName
-  property :first_name, :from => :FirstName
-  property :last_name, :from => :LastName
   property :id
   property :start_date, :from => :StartDate
   property :stop_date, :from => :StopDate
