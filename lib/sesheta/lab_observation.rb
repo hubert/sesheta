33702f18779314dd521552fc82c91baf318a5b2a
Hubert Huang <hhuang@practicefusion.com>
2013-09-16 22:58:53 -0700

    Add lab_result_id, lab_panel_id, lab_panel_sequence, lab_order_sequence properties; aliases for LabObservationID and LoincID

--- lib/sesheta/lab_observation.rb
+++ lib/sesheta/lab_observation.rb
@@ -1,6 +1,8 @@
 class Sesheta::LabObservation < Hashie::Trash
   property :id, :from => :LabObservationID
+  property :id, :from => :LabObservationId
   property :loinc_id, :from => :LoincID
+  property :loinc_id, :from => :LoincId
   property :hl7_text, :from => :HL7Text
   property :observation_value, :from => :ObservationValue
   property :units, :from => :Units
@@ -8,4 +10,9 @@ class Sesheta::LabObservation < Hashie::Trash
   property :result_status, :from => :ResultStatus
   property :is_abnormal, :from => :IsAbnormal
   property :abnormal_flags, :from => :AbnormalFlags
+
+  property :lab_result_id, :from => :LabResultId
+  property :lab_panel_id, :from => :LabPanelId
+  property :lab_panel_sequence, :from => :LabPanelSequence
+  property :sequence, :from => :LabOrderSequence
 end
--- spec/lab_observation_spec.rb
+++ spec/lab_observation_spec.rb
@@ -3,21 +3,21 @@ require 'spec_helper'
 describe Sesheta::LabObservation do
   let(:lab_observation) {
      Sesheta::LabObservation.new(
-       "LabObservationID"=>22694,
-       "LoincID"=>nil,
        "HL7Text"=>"Serum",
        "ObservationValue"=>"wtf",
        "Units"=>"units",
        "ReferenceRange"=>"None",
        "ResultStatus"=>"Final",
        "IsAbnormal"=>true,
-       "AbnormalFlags"=>"Abnormal"
+       "AbnormalFlags"=>"Abnormal",
+       "LabResultId" => 10,
+       "LabPanelId" => 5,
+       "LabPanelSequence" => 3,
+       "LabOrderSequence" => 4
      )
    }
 
   it 'adds ruby style accessors' do
-    expect(lab_observation.id).to eql(22694)
-    expect(lab_observation.loinc_id).to eql(nil)
     expect(lab_observation.hl7_text).to eql('Serum')
     expect(lab_observation.observation_value).to eql('wtf')
     expect(lab_observation.units).to eql('units')
@@ -25,5 +25,31 @@ describe Sesheta::LabObservation do
     expect(lab_observation.result_status).to eql('Final')
     expect(lab_observation.is_abnormal).to eql(true)
     expect(lab_observation.abnormal_flags).to eql('Abnormal')
+    expect(lab_observation.lab_result_id).to eql(10)
+    expect(lab_observation.lab_panel_id).to eql(5)
+    expect(lab_observation.lab_panel_sequence).to eql(3)
+    expect(lab_observation.sequence).to eql(4)
   end
+
+  context 'id' do
+    it 'can be set using LoincID' do
+      expect(Sesheta::LabObservation.new("LoincID" => 22694).loinc_id).to eql(22694)
+    end
+    
+    it 'can be set using LoincId' do
+      expect(Sesheta::LabObservation.new("LoincId" => 22694).loinc_id).to eql(22694)
+    end
+  end
+
+  context 'loinc_id' do
+    it 'can be set using LabObservationID' do
+      expect(Sesheta::LabObservation.new("LabObservationID" => 22694).id).to eql(22694)
+    end
+    
+    it 'can be set using LabObservationId' do
+      expect(Sesheta::LabObservation.new("LabObservationId" => 22694).id).to eql(22694)
+    end
+  end
+
+  
 end
