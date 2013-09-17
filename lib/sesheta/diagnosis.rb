0599db4e6a2d53f1ecf86c8102e6c3a8c45821ef
Hubert Huang <hhuang@practicefusion.com>
2013-09-16 17:51:48 -0700

    Add alias for Visit properties first name and last name

--- lib/sesheta/diagnosis.rb
+++ lib/sesheta/diagnosis.rb
@@ -6,4 +6,14 @@ class Sesheta::Diagnosis < Hashie::Trash
   property :id
   property :start_date, :from => :StartDate
   property :stop_date, :from => :StopDate
+
+  # these are here to support the legacy system for now
+  # where diagnoses and diagnoses by visit return different structured data
+  property :provider_first_name, from: :ProviderFirstName
+  property :provider_first_name, :from => :FirstName
+  property :provider_last_name, from: :ProviderLastName
+  property :provider_last_name, :from => :LastName
+  property :active, from: :Active
+  property :days_apart, from: :DaysApart
+  property :date_of_service, from: :TranscriptDOS, with: ->(prop) { prop.to_date }
 end
--- spec/diagnosis_spec.rb
+++ spec/diagnosis_spec.rb
@@ -1,24 +1,48 @@
 require 'spec_helper'
 
-describe Sesheta::Diagnosis do
-  it 'adds ruby style accessors' do
-    now = Time.now
-    diagnosis = Sesheta::Diagnosis.new(
-      "ICD"=>"642.13",
-      "DxName"=>"Hypertension secondary",
-      "FirstName"=>"Mary",
-      "LastName"=>"McPoppin",
-      "id"=>77458,
-      "StartDate"=> now,
-      "StopDate"=> now
-    )
-     
-    expect(diagnosis.icd).to eql('642.13')
-    expect(diagnosis.dx_name).to eql('Hypertension secondary')
-    expect(diagnosis.first_name).to eql('Mary')
-    expect(diagnosis.last_name).to eql('McPoppin')
-    expect(diagnosis.id).to eql(77458)
-    expect(diagnosis.start_date).to eql(now)
-    expect(diagnosis.stop_date).to eql(now)
+module Sesheta
+  describe Diagnosis do
+    it 'adds ruby style accessors' do
+      now = Time.now
+      diagnosis = Diagnosis.new(
+        "ICD"=>"642.13",
+        "DxName"=>"Hypertension secondary",
+        "id"=>77458,
+        "StartDate"=> now,
+        "StopDate"=> now,
+        "Active" => true,
+        "DaysApart" => 10,
+        "TranscriptDOS" => Time.parse('2013-05-13 09:00:01 UTC')
+      )
+       
+      expect(diagnosis.icd).to eql('642.13')
+      expect(diagnosis.dx_name).to eql('Hypertension secondary')
+      expect(diagnosis.id).to eql(77458)
+      expect(diagnosis.start_date).to eql(now)
+      expect(diagnosis.stop_date).to eql(now)
+      expect(diagnosis.active).to eql(true)
+      expect(diagnosis.days_apart).to eql(10)
+      expect(diagnosis.date_of_service).to eql(Date.parse('2013-05-13'))
+    end
+    
+    context 'provider first name' do
+      it 'can be set using ProviderFirstName' do
+        expect(Diagnosis.new('ProviderFirstName' => 'foo').provider_first_name).to eql('foo')
+      end
+      
+      it 'can be set using FirstName' do
+        expect(Diagnosis.new('FirstName' => 'foo').provider_first_name).to eql('foo')
+      end
+    end
+
+    context 'provider last name' do
+      it 'can be set using ProviderLastName' do
+        expect(Diagnosis.new('ProviderLastName' => 'foo').provider_last_name).to eql('foo')
+      end
+      
+      it 'can be set using LastName' do
+        expect(Diagnosis.new('LastName' => 'foo').provider_last_name).to eql('foo')
+      end
+    end
   end
 end
