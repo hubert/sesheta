7089ec1c987d9e56555954023d897fbecfddf630
Hubert Huang <hhuang@practicefusion.com>
2013-09-16 17:44:32 -0700

    Add aliases for Medication properties first name, last name, and id

--- lib/sesheta/medication.rb
+++ lib/sesheta/medication.rb
@@ -2,13 +2,18 @@ module Sesheta
   class Medication < Hashie::Trash
     property :name, :from => :MedName
     property :strength, :from => :Strength
-    property :id, :from => :MedId
-    property :first_name, :from => :FirstName
-    property :last_name, :from => :LastName
     property :sig_provider, :from => :SigProvider
     property :sig_patient, :from => :SigPatient
     property :connection
     property :prescriptions
+
+    # these props have two names depending on which db procedure endpoint you are hitting....
+    property :id, :from => :MedId
+    property :id, :from => :MedicationID
+    property :provider_first_name, :from => :FirstName
+    property :provider_first_name, :from => :ProviderFirstName
+    property :provider_last_name, :from => :LastName
+    property :provider_last_name, :from => :ProviderLastName
 
     def prescriptions
       self[:prescriptions] || 
--- spec/medication_spec.rb
+++ spec/medication_spec.rb
@@ -1,23 +1,51 @@
 require 'spec_helper'
 
-describe Sesheta::Medication do
-  it 'adds ruby style accessors' do
-    med = Sesheta::Medication.new(
-      "MedName" => "Tylenol",
-      "Strength"=>"varying",
-      "MedId"=>125011,
-      "FirstName"=>"mary",
-      "LastName"=>"McPoppin",
-      "SigProvider"=>"do not show",
-      "SigPatient"=>"do not show"
-    )
+module Sesheta
+  describe Medication do
+    it 'adds ruby style accessors' do
+      med = Sesheta::Medication.new(
+        "MedName" => "Tylenol",
+        "Strength"=>"varying",
+        "FirstName"=>"mary",
+        "LastName"=>"McPoppin",
+        "SigProvider"=>"do not show",
+        "SigPatient"=>"do not show"
+      )
 
-    expect(med.name).to eql('Tylenol')
-    expect(med.strength).to eql('varying')
-    expect(med.id).to eql(125011)
-    expect(med.first_name).to eql('mary')
-    expect(med.last_name).to eql('McPoppin')
-    expect(med.sig_provider).to eql('do not show')
-    expect(med.sig_patient).to eql('do not show')
+      expect(med.name).to eql('Tylenol')
+      expect(med.strength).to eql('varying')
+      expect(med.sig_provider).to eql('do not show')
+      expect(med.sig_patient).to eql('do not show')
+    end
+    
+    context 'id' do
+      it 'can be set using MedId' do
+        expect(Medication.new(:MedId => 1).id).to eql(1)
+      end
+
+      it 'can be set using MedicationID' do
+        expect(Medication.new(:MedicationID => 1).id).to eql(1)
+      end
+    end
+
+    context 'provider_first_name' do
+      it 'can be set using FirstName' do
+        expect(Medication.new(:FirstName => 'foo').provider_first_name).to eql('foo')
+      end
+
+      it 'can be set using ProviderFirstName' do
+        expect(Medication.new(:ProviderFirstName => 'foo').provider_first_name).to eql('foo')
+      end
+    end
+
+    context 'provider_last_name' do
+      it 'can be set using LastName' do
+        expect(Medication.new(:LastName => 'foo').provider_last_name).to eql('foo')
+      end
+
+      it 'can be set using ProviderLastName' do
+        expect(Medication.new(:ProviderLastName => 'foo').provider_last_name).to eql('foo')
+      end
+    end
   end
 end
